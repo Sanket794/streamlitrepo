402f8a22482f1b861decf0f13a5e052ff549129e
Sanket794 <107101283+Sanket794@users.noreply.github.com>
2024-03-23 10:13:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,8 @@ with st.container():
     st.write("---")
     left_column, right_column = st.columns(2)
     with left_column:
-        for i in range(0,3):
-            st.header("Whta I do")
-            st.write("##")
-            st.write("""It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).""")
-            st.write("[see more](https://github.com/Sanket794/streamlitrepo/blob/main/app.py)")
+        st.header("Whta I do")
+        st.write("##")
+        st.write("""It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).""")
+        st.write("[see more](https://github.com/Sanket794/streamlitrepo/blob/main/app.py)")
 
