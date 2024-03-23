3f0acb757cbf050650b834b97848502e501d7287
Sanket794 <107101283+Sanket794@users.noreply.github.com>
2024-03-23 12:14:32 +0530

    changes

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import requests
 import streamlit as st
-
+from stream_lottie import st_lottie
 st.set_page_config(page_title = "My page",page_icon =":tada:",layout = "wide")
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+#---- LOAD ASSERTS-----
+lot_code = load_lottieurl("C:/Users/verma/OneDrive/Documents/GitHub/streamlitrepo/animations")
 
 #------Header-------
 with st.container():
@@ -15,4 +23,7 @@ with st.container():
         st.write("##")
         st.write("""It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).""")
         st.write("[see more](https://github.com/Sanket794/streamlitrepo/blob/main/app.py)")
+    with right_column:
+        st_lottie(load_lottieurl,height = 300, key = "coding")
+    
 
