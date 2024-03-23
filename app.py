d6e4e058ba12094e7466c0dc86fea520e0310558
Sanket794 <107101283+Sanket794@users.noreply.github.com>
2024-03-23 09:26:07 +0530

    Update app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title = "My page",page_icon =":tada:",layout = "wide")
+
+#------Header-------
+st.subheader("Hi i am sanket :wave:")
+
+st.title("A noraml human")
+st.write("I am a simple who want to test thid python libaray")
