975e5388c385f9f80d8e91ff4672b0da4c94ac7e
Sanket794 <107101283+Sanket794@users.noreply.github.com>
2024-03-23 12:59:07 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import json 
 import requests 
 
@@ -47,38 +46,3 @@ with st.container():
                   # THis is just to uniquely identify the animation 
                   key='Car'
         ) 
-=======
-import json 
-import requests 
-
-import streamlit as st 
-from streamlit_lottie import st_lottie 
-
-
-url = requests.get( 
-	"https://assets2.lottiefiles.com/packages/lf20_mDnmhAgZkb.json") 
-url_json = dict() 
-if url.status_code == 200: 
-	url_json = url.json() 
-else: 
-	print("Error in URL") 
-
-
-st.title("Adding Lottie Animation in Streamlit WebApp") 
-
-st_lottie(url_json, 
-		# change the direction of our animation 
-		reverse=True, 
-		# height and width of animation 
-		height=400, 
-		width=400, 
-		# speed of animation 
-		speed=1, 
-		# means the animation will run forever like a gif, and not as a still image 
-		loop=True, 
-		# quality of elements used in the animation, other values are "low" and "medium" 
-		quality='high', 
-		# THis is just to uniquely identify the animation 
-		key='Car'
-		) 
->>>>>>> 49d55d22cf28f43ed0505083be43512750eebfb8
